78a2162cd61eb3b7eaeeefa153092f70f29c499b
jan.brysiak <jan.brysiak@wp.pl>
2018-10-02 21:20:15 +0200

    Dodaj pliki projektów.

---
+++ SBR1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SBR1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+           
+
+            return View();
+        }
+
+        public ActionResult Lekcje()
+        {
+           
+
+            return View();
+        }
+
+        [Authorize(Users = "janeksbr1,mariuszsbr2,tomeksbr3")]
+        public ActionResult Trening()
+        {
+            return View();
+        }
+    }
+}
---
+++ SBR1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SBR1.MvcApplication" Language="C#" %>
